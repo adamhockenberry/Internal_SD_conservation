351245a40832624e6af7546c070bee2c1524c30a
Adam Hockenberry <hockenberry.adam@gmail.com>
2017-08-02 09:32:33 -0500

    random push, this is still exploratory so everything is in constant flux and not much sense making detailed commits/messages

--- Code/BASH_Scripts/phyloP_all.sh
+++ Code/BASH_Scripts/phyloP_all.sh
@@ -9,20 +9,12 @@
 #    ~/workspace/phast-master/bin/phyloP --wig-scores --refidx 0 --method GERP Reference232seqModel.mod $i > $outfile 
 #done
 
-#for i in ../../Data/Orthologs/Order_Enterobacterales_Representative/mfastas_nt/*.mfasta; do
-#    #outfile=../../Results/Order_Enterobacterales_Representative/phyloP/$(basename $i)
-#    #outfile=${outfile/mfasta/wig}
-#    #echo $outfile
-#    #~/workspace/phast-master/bin/phyloP --wig-scores --refidx 0 --method LRT Representative101seqModel.mod $i > $outfile 
-#    outfile=../../Results/Order_Enterobacterales_Representative/phyloP_CONACC/$(basename $i)
-#    outfile=${outfile/mfasta/wig}
-#    echo $outfile
-#    ~/workspace/phast-master/bin/phyloP --wig-scores --refidx 0 --method LRT --mode CONACC Representative101seqModel.mod $i > $outfile 
-#    #outfile=../../Results/Order_Enterobacterales_Representative/phyloP_GERP/$(basename $i)
-#    #outfile=${outfile/mfasta/wig}
-#    #echo $outfile
-#    #~/workspace/phast-master/bin/phyloP --wig-scores --refidx 0 --method GERP Representative101seqModel.mod $i > $outfile 
-#done
+for i in ../../Data/Orthologs/Order_Enterobacterales_Mixed/mfastas_nt/*.mfasta; do
+    outfile=../../Results/Order_Enterobacterales_Mixed/phyloP_CONACC/$(basename $i)
+    outfile=${outfile/mfasta/wig}
+    echo $outfile
+    ~/workspace/phast-master/bin/phyloP --wig-scores --refidx 0 --method LRT --mode CONACC Mixed108seqModel.mod $i > $outfile 
+done
 
 
 #for i in ../../Data/Orthologs/Order_Enterobacterales_Representative/mfastas_nt/VBIEscCol129921_268*.mfasta; do
@@ -31,4 +23,4 @@
 #    ~/workspace/phast-master/bin/phyloFit --tree ../../Data/Tree_files/Order_Enterobacterales_Representative/RAxML_bestTree.RepresentativeSet101seqsFLAT --EM --post-probs -P --out-root jussssttesting i 
 #done
 
-~/workspace/phast-master/bin/phyloFit --tree ../../Data/Tree_files/Order_Enterobacterales_Representative/RAxML_bestTree.RepresentativeSet101seqsFLAT --EM -U --out-root jussssttesting blah.fa 
+#~/workspace/phast-master/bin/phyloFit --tree ../../Data/Tree_files/Order_Enterobacterales_Representative/RAxML_bestTree.RepresentativeSet101seqsFLAT --EM -U --out-root jussssttesting blah.fa 
